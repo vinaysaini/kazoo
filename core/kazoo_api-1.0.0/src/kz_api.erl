59e6608927e78cd7eb6739810d47ad16ddeeed93
James Aimonetti <james@2600hz.org>
2013-10-20 12:05:08 -0700

    amqp_json_schema: remove couch from the equation

--- core/kazoo_api-1.0.0/src/kz_api.erl
+++ core/kazoo_api-1.0.0/src/kz_api.erl
@@ -19,31 +19,17 @@ build_message(JObj) ->
     wh_json:encode(JObj).
 
 find_schema(SchemaName) ->
-    try couch_mgr:open_cache_doc(<<"system_amqp_schemas">>, SchemaName) of
-        {'ok', Schema} -> {'ok', wh_json:merge_jobjs(Schema, ?DEFAULT_HEADERS_SCHEMA)};
+    case wh_cache:fetch(SchemaName) of
+        {'ok', _Schema}=OK -> OK;
         {'error', 'not_found'} ->
-            lager:debug("~s not found in couch, looking on disk", [SchemaName]),
-            maybe_find_schema_in_priv(SchemaName);
-        {'error', _E}=E ->
-            lager:debug("failed to find AMQP schema ~s: ~p", [SchemaName, _E]),
-            E
-    catch
-        _E:'undefined' ->
-            lager:debug("couch_mgr appears unavailable due to ~s", [_E]),
-            maybe_find_schema_in_priv(SchemaName);
-        _E:_R ->
-            lager:debug("couch_mgr excepted: ~s: ~p", [_E, _R]),
-            maybe_find_schema_in_priv(SchemaName)
-    end.
-
-maybe_find_schema_in_priv(SchemaName) ->
-    case code:priv_dir('kazoo_api') of
-        {'error', 'bad_name'} ->
-            lager:debug("kazoo_api/priv missing or not found"),
-            {'error', 'not_found'};
-        Path ->
-            lager:debug("found ~s in ~s", [SchemaName, Path]),
-            import_schema(Path ++ ["/", SchemaName, ".json"])
+            case code:priv_dir('kazoo_api') of
+                {'error', 'bad_name'} ->
+                    lager:debug("kazoo_api/priv missing or not found"),
+                    {'error', 'not_found'};
+                Path ->
+                    lager:debug("found ~s in ~s", [SchemaName, Path]),
+                    import_schema(Path ++ ["/", SchemaName, ".json"])
+            end
     end.
 
 import_schema(Path) when is_list(Path) ->
@@ -51,27 +37,25 @@ import_schema(Path) when is_list(Path) ->
 import_schema(Path) ->
     SchemaName = filename:basename(Path, ".json"),
 
-    case wh_cache:fetch(SchemaName) of
-        {'ok', _Schema}=OK ->
-            lager:debug("found schema ~s in cache: '~p'", [SchemaName, _Schema]),
-            OK;
-        {'error', 'not_found'} ->
-            {'ok', Bin} = file:read_file(Path),
-            try wh_json:decode(Bin) of
-                Schema ->
-                    Properties = wh_json:get_value(<<"properties">>, Schema, wh_json:new()),
-                    MergedProperties = wh_json:merge_jobjs(Properties, ?DEFAULT_HEADERS_SCHEMA),
-                    MergedSchema = wh_json:set_value(<<"properties">>, MergedProperties, Schema),
-                    lager:debug("caching ~s", [SchemaName]),
-                    wh_cache:store(SchemaName, MergedSchema),
-                    {'ok', MergedSchema}
-            catch
-                _E:_R ->
-                    lager:debug("failed to decode JSON: ~s: ~p", [_E, _R]),
-                    lager:debug("looked in ~s, found ~s", [Path, Bin]),
-                    {'error', 'not_found'}
-            end
+    {'ok', Bin} = file:read_file(Path),
+    try wh_json:decode(Bin) of
+        Schema ->
+            MergedSchema = merge_defaults(Schema),
+            lager:debug("caching ~s", [SchemaName]),
+            wh_cache:store(SchemaName, MergedSchema),
+            {'ok', MergedSchema}
+    catch
+        _E:_R ->
+            lager:debug("failed to decode JSON: ~s: ~p", [_E, _R]),
+            lager:debug("looked in ~s, found ~s", [Path, Bin]),
+            {'error', 'not_found'}
     end.
+
+-spec merge_defaults(wh_json:object()) -> wh_json:object().
+merge_defaults(Schema) ->
+    Properties = wh_json:get_value(<<"properties">>, Schema, wh_json:new()),
+    MergedProperties = wh_json:merge_jobjs(Properties, ?DEFAULT_HEADERS_SCHEMA),
+    wh_json:set_value(<<"properties">>, MergedProperties, Schema).
 
 init_schemas() ->
     case code:priv_dir('kazoo_api') of
