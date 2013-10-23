083e028e1765bbec3af54cf36fba3095a65e189e
James Aimonetti <james@2600hz.org>
2013-10-23 07:11:36 -0700

    amqp_json_schema: add req/2 to allow custom options to be sent to validator

--- core/kazoo_api-1.0.0/src/kapi_authn.erl
+++ core/kazoo_api-1.0.0/src/kapi_authn.erl
@@ -8,25 +8,32 @@
 %%%-------------------------------------------------------------------
 -module(kapi_authn).
 
--export([req/1, req_v/1]).
+-export([req/1, req/2, req_v/1, req_v/2]).
 
 -include("kazoo_api.hrl").
 
 -spec req(api_terms()) ->
                  {'ok', iolist()} |
                  {'error', string()}.
-req(Prop) when is_list(Prop) ->
-    req(wh_json:from_list(Prop));
-req(JObj) ->
-    case req_v(JObj) of
-        {'ok', FixedJObj} -> kz_api:build_message(FixedJObj);
+req(API) -> req(API, []).
+
+req(Prop, Options) when is_list(Prop) ->
+    req(wh_json:from_list(Prop), Options);
+req(JObj, Options) ->
+    case req_v(JObj, Options) of
+        {'ok', FixedJObj} ->
+            lager:debug("end validate"),
+            kz_api:build_message(FixedJObj);
         {'error', Errors} -> {'error', Errors};
         'false' -> {'error', "API failed validation for authn_req"}
     end.
 
 -spec req_v(api_terms()) -> boolean().
-req_v(Prop) when is_list(Prop) ->
-    req_v(wh_json:from_list(Prop));
-req_v(JObj) ->
+req_v(API) -> req_v(API, []).
+
+req_v(Prop, Options) when is_list(Prop) ->
+    req_v(wh_json:from_list(Prop), Options);
+req_v(JObj, Options) ->
     {'ok', Schema} = kz_api:find_schema(<<"authn_req">>),
-    jesse:validate_with_schema(Schema, JObj).
+    lager:debug("start validate"),
+    jesse:validate_with_schema(Schema, JObj, [{'allowed_errors', 2} | Options]).
